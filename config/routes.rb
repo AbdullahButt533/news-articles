d5777389841b685ffceea095f8d906b1d40d7e91
abdullah <butt.abdullah.705@gmail.com>
2023-04-10 03:18:42 +0500

    Configure Devise Token Auth Added Role and Admin Request Status Fields

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'api/auth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
