655ab8e7b977d50c9aba27b54961d856f2f293bb
abdullah <butt.abdullah.705@gmail.com>
2023-04-12 06:12:58 +0500

    Database Updates for Topics Table Topic Model Added

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_09_213700) do
+ActiveRecord::Schema.define(version: 2023_04_12_010132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "topics", force: :cascade do |t|
+    t.string "title", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["title"], name: "index_topics_on_title", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
