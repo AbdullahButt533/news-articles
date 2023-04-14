d3e636c96ac12469e1c2ad5b90381f654847831a
abdullah <butt.abdullah.705@gmail.com>
2023-04-15 04:50:13 +0500

    Added Sidekiq Configure Redis Refactor Search Service

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Article < ApplicationRecord
+  EVERYTHING_PATH = 'everything'
+  TOP_HEADLINES_PATH = 'top-headlines'
+
   validates :url, presence: true, uniqueness: true
   validates :description, length: { maximum: 500 }
   validates :content, length: { maximum: 500 }
